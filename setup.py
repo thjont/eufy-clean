d8b8dc1b773b9187f6b9346de9a2578c67c1eebf
Tobias Jeppesen <github@tobiasjeppesen.com>
2025-01-30 16:38:12 +0100

    Hacs support?

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+dynamic_requires = []
+
+setup(
+    name="robovac_mqtt",
+    version="1.0",
+    url="http://github.com/jeppesens/eufy-clean",
+    packages=find_packages(),
+    scripts=[],
+    description="Python API for controlling Eufy Robovac X10 Pro Omni",
+    classifiers=[],
+
+)
